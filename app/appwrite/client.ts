4b9b0cdf390a35370d02bc1a0c34382cf309f49e
AndyT05 <thanhan22595@gmail.com>
2025-09-12 12:37:37 -0500

    Implement Authentication

---
+++ app/appwrite/client.ts
@@ -0,0 +1,18 @@
+import { Client, TablesDB, Account, Storage } from "appwrite";
+
+export const appwriteConfig = {
+  endpointUrl: import.meta.env.VITE_APPWRITE_API_ENDPOINT,
+  projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
+  apikey: import.meta.env.VITE_APPWRITE_API_KEY,
+  databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
+  userCollectionId: import.meta.env.VITE_APPWRITE_USERS_COLLECTION_ID,
+  tripCollectionId: import.meta.env.VITE_APPWRITE_TRIPS_COLLECTION_ID,
+};
+
+export const client = new Client()
+  .setEndpoint(appwriteConfig.endpointUrl)
+  .setProject(appwriteConfig.projectId);
+
+export const database = new TablesDB(client);
+export const account = new Account(client);
+export const storage = new Storage(client);
